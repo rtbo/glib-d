0c6e67ac39fde2ba92a57881f624559657a1d00f
Remi Thebault <remi.thebault@outlook.com>
2015-03-13 07:23:18 +0100

    add license information

--- source/glib/gnode.d
+++ source/glib/gnode.d
@@ -1,3 +1,8 @@
+/*
+ * Distributed under the Boost Software License, Version 1.0.
+ *    (See accompanying file LICENSE_1_0.txt or copy at
+ *          http://www.boost.org/LICENSE_1_0.txt)
+ */
 module glib.gnode;
 
 import glib.gmem;
@@ -30,7 +35,7 @@ extern (C) {
     alias GNodeTraverseFunc = gboolean function (GNode *node, gpointer data);
 
     alias GNodeForeachFunc = void function (GNode *node, gpointer data);
-                             
+
     alias GCopyFunc = gpointer function (gconstpointer src, gpointer data);
 
 }
